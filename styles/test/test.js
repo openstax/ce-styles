570515730ef802fa888bb469954ddf7d1dae6623
tomjw64 <tom.jw64@gmail.com>
2019-04-16 18:42:46 -0500

    Add failing enum creation test

--- styles/test/test.js
+++ styles/test/test.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const sassTrue = require('sass-true')
+const globImporter = require('node-sass-glob-importer');
 
 const frameworkIncludesPath = `${__dirname}/../framework`
 const sassFile = path.join(__dirname, 'test-framework.scss')
 
 sassTrue.runSass({
   file: sassFile,
-  includePaths: [frameworkIncludesPath]
+  importer: [globImporter()],
+  includePaths: [frameworkIncludesPath],
 }, describe, it)
