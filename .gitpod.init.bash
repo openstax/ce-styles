c33b81b11cc3ef0f496867f2619c6cb41a0868df
Jennifer Wilson <26155528+jbwilson8@users.noreply.github.com>
2023-11-02 13:25:31 -0500

    Revert ":bug: fix gitpod install and dart files"
    This reverts commit 649a10c899a0bd8f7bd3d90f610814b43c0b0644.

--- .gitpod.init.bash
+++ .gitpod.init.bash
@@ -7,7 +7,8 @@ yarn install
 # Install old version of dart
 brew tap dart-lang/dart
 brew install dart@$DART_VERSION
-brew link dart@$DART_VERSION
+echo "export PATH='/home/linuxbrew/.linuxbrew/opt/dart@$DART_VERSION/bin:\$PATH'" >> $HOME/.bash_profile
+source $HOME/.bash_profile
 
 # Install dart dependencies
 dart pub get
