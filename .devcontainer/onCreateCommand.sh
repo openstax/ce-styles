e85abecfdad05b088ac9046d01205654a198ee23
Kendra Swanson <kendraswan@gmail.com>
2024-07-17 15:34:57 +0000

    write build.js and update deps

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -4,15 +4,3 @@ if [ ! -d node_modules ]; then
   echo "Installing node dependencies ..."
   yarn install
 fi
-
-echo "Installing dart dependencies ..."
-dart pub get
-
-## It's common to get a network error when trying to install dart deps.
-if [[ $? == 69 ]]; then
-  echo "ATTENTION!!! Dart dependencies have failed to install due to a network error."
-  echo "To troubleshoot, re-run 'dart pub get' in the terminal until it succeeds."
-  echo "If command continues to fail, try switching to a VPN."
-fi
-
-exit 0
---
+++ styles/build/build.js
@@ -0,0 +1,83 @@
+let start = performance.now();
+const sass = require('sass-embedded');
+const fs = require('fs');
+let finish = performance.now();
+
+console.log(`Loaded imports in ${ Math.round(finish - start) / 1000}s.`);
+
+// compiles a single style with given input & output paths
+const compileAStyle = (path, outputPath) => {
+  // compile with metrics
+  let start = performance.now();
+  const result = sass.compile(
+    path,
+    {
+      loadPaths: [ '', 'styles/'],
+      sourceMap: true,
+      functions: {
+        'toDataUri($type, $path)': function(args) {
+          // TODO
+          return new sass.SassString('hello');
+        }
+      }
+    }
+  );
+  let finish = performance.now();
+  console.log(`compile time is ${ Math.round(finish - start) / 1000} seconds`);
+
+  // write
+  fs.writeFile(outputPath, result.css, (err) => { if (err) throw err; });
+}
+
+// given a book keyword, gets input & output paths
+const getPaths = (book) => {
+  let bookPathBase = "styles/books/";
+  let outPathBase = "styles/output/";
+  let bookPath = '';
+  let outPath = '';
+  if (['generic', 'web', 'webview', 'webview-generic'].includes(book)) {
+    bookPath = `${bookPathBase}generic/webview.scss`;
+    outPath = `${outPathBase}webview-generic.css`;
+  }
+  else {
+    bookPath = `${bookPathBase}${book}/book.scss`;
+    outPath = `${outPathBase}${book}-pdf.css`;
+  }
+  return { bookPath, outPath };
+}
+
+// searches book files for instances of theme keyword.
+// if at least one example is found, book is added to array & returned.
+const booksByTheme = (theme) => {
+  booksInTheme = []
+  fs.readdirSync("styles/books/").forEach((book) => {
+    let { bookPath, _ } = getPaths(book);
+    let themePos = fs.readFileSync(bookPath, 'utf8').search(`${theme}:::`);
+    if (themePos !== -1) {
+      booksInTheme.push(book);
+    }
+  });
+  // special case - ap-physics just imports college physics, but needs to be compiled.
+  if (theme === 'carnival') { booksInTheme.push ('ap-physics'); }
+  return booksInTheme;
+}
+
+const main = (args) => {
+  if (args.length === 0) {
+    // if no books given, compile all books
+    args = fs.readdirSync("styles/books/");
+  } else if (['cardboard', 'carnival', 'corn', 'cosmos'].includes(args[0])) {
+    // if theme keyword given, find all books with instances of theme
+    args = booksByTheme(args[0]);
+  }
+  // compile styles for all books
+  let total = args.length;
+  console.log(`Compiling styles for ${total} books...`);
+  args.forEach((book, index) => {
+    let { bookPath, outPath } = getPaths(book);
+    console.log(`${index+1}/${total} ${bookPath} -> ${outPath}`);
+    compileAStyle(bookPath, outPath);
+  })
+}
+
+main(process.argv.slice(2));
