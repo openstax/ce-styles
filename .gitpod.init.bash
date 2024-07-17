d387b0d9bda76355711f6da8283fb0f5f39b942d
Kendra Swanson <kendraswan@gmail.com>
2024-07-17 19:38:59 +0000

    remove dart and update readme

--- .gitpod.init.bash
+++ .gitpod.init.bash
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-DART_VERSION=2.19
-
 yarn install
-
-# Install old version of dart
-brew tap dart-lang/dart
-brew install dart@$DART_VERSION
-brew link dart@$DART_VERSION
-
-# Install dart dependencies
-dart pub get
